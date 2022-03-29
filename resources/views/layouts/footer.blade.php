d959c658e1cd62424ce96acad7a36859249c6811
AliSami6 <alisamicse320@gmail.com>
2022-03-29 20:35:04 +0600

    Social Media project

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="footer no-shadow">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                &copy; FaceClone 2022.
+            </div>
+        </div>
+    </div>
+</div>
